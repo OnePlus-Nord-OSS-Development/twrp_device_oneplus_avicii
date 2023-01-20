6d4fb2c8a43dc392b8eb49fc3f132d0c97f4c657
Sreeshankar K <sreeshankar0910@gmail.com>
2023-01-20 15:30:00 +0530

    avicii: Exclude twrp app
    Signed-off-by: Sreeshankar K <sreeshankar0910@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,6 +155,7 @@ TW_Y_OFFSET := 107
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone94/temp"
 TW_EXCLUDE_APEX := true
+TW_EXCLUDE_TWRPAPP := true
 TW_DEFAULT_BRIGHTNESS := 200
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
