6965f18de52e57c53da499eec0db4a67bd2dbe9d
Sreeshankar K <sreeshankar0910@gmail.com>
2023-01-20 15:30:00 +0530

    avicii: Add video kernel cmdline
    Signed-off-by: Sreeshankar K <sreeshankar0910@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,6 +56,7 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.hardware=qcom \
     androidboot.console=ttyMSM0 \
     androidboot.memcg=1 \
+    video=vfb:640x400,bpp=32,memsize=3072000 \
     lpm_levels.sleep_disabled=1 \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
